c14e29e95649c365334e9c8a1b5bd90282ff3175
Douglas Narcizo <144618521+douglas-narcizo@users.noreply.github.com>
2025-03-21 10:54:33 -0300

    Enhance product specials logic

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -20,16 +20,47 @@ const productReducer = (state, action) => {
   }
 };
 
+/**
+ * The following accessory logic was created only to randomly select
+ * daily specials for demonstration purposes.
+ * In real life, specials are set by real people from the store's team
+ * and this logic must be removed.
+ */
+const sortByCategoryRoundRobin = (productsList) => {
+  const categoryMap = productsList.reduce((map, product) => {
+    if (!map[product.category]) {
+      map[product.category] = [];
+    }
+    map[product.category].push(product);
+    return map;
+  }, {});
+
+  const sortedProducts = [];
+  const categoryKeys = Object.keys(categoryMap);
+
+  while (categoryKeys.some((key) => categoryMap[key].length > 0)) {
+    for (const key of categoryKeys) {
+      if (categoryMap[key].length > 0) {
+        sortedProducts.push(categoryMap[key].shift());
+      }
+    }
+  }
+
+  return sortedProducts;
+};
+
 export const ProductProvider = ({ children }) => {
   const [state, dispatch] = useReducer(productReducer, initialState);
 
   useEffect(() => {
     const fetchProducts = async () => {
       const productsList = await getProductsList(); // getProductsList(state.category);
-      const products = shuffleArray(productsList);
-      for (let i = 0; i < products.length; i++) {
-        products[i].special = i < (products.length * 0.4);
+      let products = shuffleArray(productsList);
+      const sortedProducts = sortByCategoryRoundRobin(products);
+      for (let i = 0; i < sortedProducts.length; i++) {
+        sortedProducts[i].special = i < (sortedProducts.length * 0.4);
       }
+      products = shuffleArray(sortedProducts);
       dispatch({ type: 'SET_PRODUCTS', payload: products });
     };
 

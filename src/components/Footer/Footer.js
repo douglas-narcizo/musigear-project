42ff819a47018286a7d13aea7baf01e44cd3a52e
Douglas Narcizo <144618521+douglas-narcizo@users.noreply.github.com>
2025-03-24 21:10:12 -0300

    Add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const Footer = () => {
+  return (
+    <Box 
+      component='footer' 
+      sx={{
+        justifyItems: 'center',
+        alignItems: 'center',
+        padding: '1rem',
+        backgroundColor: '#f8f9fa',
+        borderTop: '1px solid #e9ecef',
+        marginTop: '2rem',
+      }}
+    >
+      <Stack direction='row' justifyContent='center' color='primary.main'>
+        <Typography variant='body2' letterSpacing={2} mt='2px' sx={{fontStyle: 'italic'}}>
+          Developed by
+        </Typography>
+        <Typography variant='body2' fontWeight={500} mx='4px' mt='2px'>
+          - Douglas Narcizo | 2025
+        </Typography>
+        <Link 
+          href='https://github.com/douglas-narcizo' 
+          target='_blank' 
+          rel='noopener noreferrer' 
+          sx={{ mx: '0.5rem', p: 0, textDecoration: 'none' }}
+        >
+          <GitHubIcon />
+        </Link>
+        <Link 
+          href='https://www.linkedin.com/in/douglas-narcizo' 
+          target='_blank' 
+          rel='noopener noreferrer' 
+          sx={{ mx: '0.5rem', textDecoration: 'none' }}
+        >
+          <LinkedInIcon />
+        </Link>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
